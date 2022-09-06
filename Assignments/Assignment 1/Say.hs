38c26f75d0dc03fd5e76414340103639e28cee74
NemuruYama <nemuruyama@gmail.com>
2022-09-06 15:15:54 +0200

    Finished 1.6, non-optional

--- Assignments/Assignment 1/Say.hs
+++ Assignments/Assignment 1/Say.hs
@@ -1,5 +1,17 @@
 module Say where
 
+type Group = (AboveThousand, BelowThousand)
+
+type AboveThousand = Integer
+type BelowThousand = Integer
+
+-- toGroups --> get the group before thousand and after thousand
+toGroups :: Integer -> Group
+toGroups n = if n >= 10^6 then undefined else (n `div` 1000, n `mod` 1000)
+
+condiftionalSay :: Integer -> String
+condiftionalSay n = if n > 0 then " " ++ say n else ""
+
 say :: Integer -> String
 say  0 = "zero"
 say  1 = "one"
@@ -19,38 +31,20 @@ say 15 = "fifteen"
 say 18 = "eighteen"
 say 20 = "twenty"
 say 30 = "thirty"
-say 40 = "fourty"
+say 40 = "forty"
 say 50 = "fifty"
 say 60 = "sixty"
 say 70 = "seventy"
 say 80 = "eighty"
 say 90 = "ninety"
 
--- say 100 => one hundred (1 100)
--- <= million
-
--- `mod` 10 => get first digit
--- `mod` 100 => get second digit
--- `mod` 1000 => get third digit
--- `mod` 10000 => get fourth digit
--- `mod` 100000 => get fifth digit
--- `mod` 1000000 => get sixth digit
-
 say n
-    | n > 12 && n < 20 = say first ++ "teen"
-    | n < 100 = say (second * 10) ++ " " ++ say first
-    | n < 1000 = say third ++ " hundred " ++ say (n `mod` 100)
-    | n < 1000000 = say fourth ++ " thousand " ++ say (n `mod` 1000)
-    | otherwise = say n
-    where 
-        first = n `mod` 10
-        second = n `mod` 100 `div` 10
-        third = n `mod` 1000 `div` 100
-        fourth = n `mod` 10000 `div` 1000
-        fifth = n `mod` 100000 `div` 10000
-        sixth = n `mod` 1000000 `div` 100000
-        
-
-
+    | n > 12 && n < 20 = say (n `mod` 10) ++ "teen"
+    | n < 100 = say ((n `mod` 100 `div` 10) * 10) ++ condiftionalSay (n `mod` 10)
+    | n < 1000 = say (n `mod` 1000 `div` 100) ++ " hundred" ++ condiftionalSay (n `mod` 100)
+    | otherwise = say above ++ " thousand" ++ condiftionalSay below
+        where 
+            (above, below) = toGroups n
 
 -- 999999 => (nine hundred) (ninety) (nine) (thousand) (nine hundred) (ninety) (nine)
+--        => (group 1) (thousand) (group 2)
