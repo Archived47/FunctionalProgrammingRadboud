61a4f077d3f29d06771a64066b304f4269819a06
NemuruYama <nemuruyama@gmail.com>
2022-09-20 10:27:45 +0200

    Exercise 3.3

--- Assignments/Assignment 3/ListFunctions.hs
+++ Assignments/Assignment 3/ListFunctions.hs
@@ -1,9 +1,27 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 module ListFunctions where
 
 import Prelude hiding (or, and, elem, take, drop)
 
---and :: [Bool] -> Bool
---or :: [Bool] -> Bool
---elem :: (Eq a) => a -> [a] -> Bool
---drop :: Int -> [a] -> [a]
---take :: Int -> [a] -> [a]
+and :: [Bool] -> Bool
+and [] = True
+and (x:xs) = x && and xs
+
+or :: [Bool] -> Bool
+or [] = True
+or (x:xs) = x || and xs
+
+elem :: (Eq a) => a -> [a] -> Bool
+elem el [] = False
+elem el (x:xs) = el == x || elem el xs
+
+drop :: Int -> [a] -> [a]
+drop n [] = []
+drop 0 xs = xs
+drop n (x:xs) = drop (n-1) xs
+
+take :: Int -> [a] -> [a]
+take n [] = []
+take 0 xs = []
+take n (x:xs) = x : take (n-1) xs
