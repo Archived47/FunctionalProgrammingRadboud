f23249470ade794a7df54844a8a6a6a1e1bea448
NemuruYama <nemuruyama@gmail.com>
2022-09-20 12:09:53 +0200

    Exercise 3.7

--- Assignments/Assignment 3/Lego.hs
+++ Assignments/Assignment 3/Lego.hs
@@ -1,10 +1,29 @@
 module Lego where
 
 import Data.List
-import Data.Tuple
+import Data.Tuple ()
 
---removeAt :: Int -> [a] -> [a]
+removeAt :: Int -> [a] -> [a]
+removeAt n [] = []
+removeAt n xs = [x | (i, x) <- zip [1..] xs, i /= n]
 
---sortWithPos :: (Ord a) => [a] -> [(a,Int)]
+sortWithPos :: (Ord a) => [a] -> [(a,Int)]
+sortWithPos [] = []
+sortWithPos xs = [(x,i) | (x,i) <- sortBy sortHelper indexPairs]
+  where
+    indexPairs = [(x, i) | (i, x) <- zip [0..] xs]
+    sortHelper (a1, b1) (a2, b2)
+      | a1 < a2 = LT
+      | a1 > a2 = GT
+      | otherwise = compare a1 a2
+-- sortWithPos xs = [(x, j) | (x, i) <- sortedWithIndex, j <- find x xs]
+--   where 
+--     sortedWithIndex = [(x, i) | (i, x) <- zip [0..] (sort xs)]
+--     find y xs = [i | (i, x) <- zip [0..] xs, y == x]
 
---sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos [] = []
+sortedPos xs = [(x, j) | x <- xs, (_, i) <- sortedWithIndex, j <- find x sortedWithIndex, i == j]
+  where 
+    sortedWithIndex = [(x, i) | (i, x) <- zip [0..] (sort xs)]
+    find y xs = [i | (i, x) <- zip [0..] (map fst xs), y == x]
