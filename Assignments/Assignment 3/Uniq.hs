a07d07fe7d64f564ef875e2fda292675f102ec4e
NemuruYama <nemuruyama@gmail.com>
2022-09-20 10:55:21 +0200

    Exercise 3.5

--- Assignments/Assignment 3/Uniq.hs
+++ Assignments/Assignment 3/Uniq.hs
@@ -1,3 +1,6 @@
 module Uniq where
 
---uniq :: (Eq a) => [a] -> [a]
+uniq :: (Eq a) => [a] -> [a]
+uniq [] = []
+uniq (x1:x2:xs) = if x1 == x2 then x1 : uniq xs else x1 : uniq (x2 : xs)
+uniq (x:xs) = x : uniq xs
