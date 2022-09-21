6e96d96497e63bf7a3e71ddef82fb292c76c8ada
NemuruYama <NemuruYama@gmail.com>
2022-09-21 12:50:24 +0200

    Worked on Obfuscate and Lego

--- Assignments/Assignment 3/Lego.hs
+++ Assignments/Assignment 3/Lego.hs
@@ -8,21 +8,19 @@ removeAt n xs = [x | (i, x) <- zip [1..] xs, i /= n]
 
 sortWithPos :: (Ord a) => [a] -> [(a,Int)]
 sortWithPos [] = []
-sortWithPos xs = [(x,i) | (x,i) <- sortBy sortHelper indexPairs]
-  where
-    indexPairs = [(x, i) | (i, x) <- zip [0..] xs]
-    sortHelper (a1, b1) (a2, b2)
-      | a1 < a2 = LT
-      | a1 > a2 = GT
-      | otherwise = compare a1 a2
--- sortWithPos xs = [(x, j) | (x, i) <- sortedWithIndex, j <- find x xs]
---   where 
---     sortedWithIndex = [(x, i) | (i, x) <- zip [0..] (sort xs)]
---     find y xs = [i | (i, x) <- zip [0..] xs, y == x]
+sortWithPos xs = sortBy (\(a,_) (b,_) -> compare a b) [(x, i) | (i, x) <- zip [0..] xs]
 
-sortedPos :: (Ord a) => [a] -> [(a,Int)]
-sortedPos [] = []
-sortedPos xs = [(x, j) | x <- xs, (_, i) <- sortedWithIndex, j <- find x sortedWithIndex, i == j]
-  where 
-    sortedWithIndex = [(x, i) | (i, x) <- zip [0..] (sort xs)]
-    find y xs = [i | (i, x) <- zip [0..] (map fst xs), y == x]
+-- sortedPos :: (Ord a) => [a] -> [(a,Int)]
+-- sortedPos [] = []
+-- sortedPos xs = [(x, j) | x <- xs, (_, i) <- sortedWithIndex xs, j <- find x (sortedWithIndex xs), i == j]
+--   where 
+--     find y xs = [i | (i, x) <- zip [0..] (map fst xs), y == x]
+
+
+-- sortedWithIndex :: (Num b, Enum b, Ord a) => [a] -> [(a, b)]
+-- sortedWithIndex xs = [(x, i) | (i, x) <- zip [0..] (sort xs)]
+
+sortedWithIndex :: (Num b, Enum b, Ord a) => [a] -> [(a, b)]
+sortedWithIndex xs = [(x, i) | (i, x) <- zip [0..] (sort xs)]
+
+main = sortedWithIndex "haskell"
--- Assignments/Assignment 3/Obfuscate.hs
+++ Assignments/Assignment 3/Obfuscate.hs
@@ -1,6 +1,6 @@
 module Obfuscate where
 
-import Data.Char ( isPunctuation )
+import Data.Char ( isPunctuation, isDigit )
 import Data.List ( sortBy )
 
 getRandomNumber :: Int -> Int
@@ -14,12 +14,23 @@ shuffle ns xs = map fst (sortBy (\(_,a) (_,b) -> compare a b) [(x, i) | (x, i) <
 
 shuffleMiddle :: String -> String
 shuffleMiddle [c] = [c]
-shuffleMiddle str = head str : shuffle randomNumbers middle ++ end
+shuffleMiddle str = head str : shuffle randomizedIndexes middle ++ end
   where
-    randomNumbers = [getRandomNumber j | j <- [1..length str]]
+    indexes = [1..length middle]
+    filled = [if isPunctuation c || isDigit c then i else 0 | (c , i) <- zip middle [1..]]
+    filteredIndexes = filter (== 0) filled
+    randomNumbers = [getRandomNumber i | i <- take (length filteredIndexes) indexes]
+    randomizedFilteredIndexes = shuffle randomNumbers [j | (i,j) <- zip filled indexes, i == 0]
+    randomizedIndexes = fill filled randomizedFilteredIndexes
     begin = init str
-    end = if isPunctuation (last str) then reverse [last str, last begin] else [last str]
     middle = if isPunctuation (last str) then (init . tail) begin else tail begin
+    end = if isPunctuation (last str) then reverse [last str, last begin] else [last str]
+
+fill :: (Eq a, Num a) => [a] -> [a] -> [a]
+fill [] [] = []
+fill xs [] = xs
+fill [] ys = ys
+fill (x:xs) (y:ys) = if x == 0 then y : fill xs ys else x : fill xs (y : ys)
 
 cambridge :: String -> String
 cambridge str = unwords [shuffleMiddle word | word <- words str]
