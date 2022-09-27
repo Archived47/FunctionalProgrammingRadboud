1a26efd998204b87ca421a7e028d88a782633f1c
NemuruYama <nemuruyama@gmail.com>
2022-09-27 17:26:39 +0200

    4.6

--- Assignments/Assignment_4/AST.hs
+++ Assignments/Assignment_4/AST.hs
@@ -1,11 +1,23 @@
 module AST where
 
---start with either:
---  data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | ...
---or:
---  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
---  infixl 6 :+:
---  infixl 7 :*:
 
+-- data Expr = Lit Integer | Add Expr Expr
+data Expr = Lit Integer | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Div Expr Expr | VarX
+  deriving (Show)
 
---eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+eval (Lit lit) x = Just (fromInteger lit)
+eval VarX x = Just x
+eval (Add e1 e2) x = case (eval e1 x, eval e2 x) of
+  (Just x, Just y) -> Just (x + y)
+  _ -> Nothing
+eval (Sub e1 e2) x = case (eval e1 x, eval e2 x) of
+  (Just x, Just y) -> Just (x - y)
+  _ -> Nothing
+eval (Mul e1 e2) x = case (eval e1 x, eval e2 x) of
+  (Just x, Just y) -> Just (x * y)
+  _ -> Nothing
+eval (Div e1 e2) x = case (eval e1 x, eval e2 x) of
+  (_, Just 0) -> Nothing
+  (Just x, Just y) -> Just (x / y)
+  _ -> Nothing
