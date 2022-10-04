20e5ab7eed8bc157ea57b7af180351f2c7971acb
NemuruYama <nemuruyama@gmail.com>
2022-10-04 14:25:43 +0200

    5.3

--- Assignments/Assignment_5/ThereCanBeOnlyOne.hs
+++ Assignments/Assignment_5/ThereCanBeOnlyOne.hs
@@ -1,5 +1,13 @@
 module ThereCanBeOnlyOne where
 
 -- onlyElem :: (Eq a) => a -> [a] -> Bool
+-- onlyElem n xs = length (filter (== n) xs) == 1
 
--- onlyOnce :: (a -> Bool) -> [a] -> Bool
+-- onlyElem :: (Eq a) => a -> [a] -> Bool
+-- onlyElem n = (== 1) . length . filter (== n)
+
+onlyOnce :: (a -> Bool) -> [a] -> Bool
+onlyOnce f = (== 1) . length . filter f
+
+onlyElem :: (Eq a) => a -> [a] -> Bool
+onlyElem n = onlyOnce (== n)
