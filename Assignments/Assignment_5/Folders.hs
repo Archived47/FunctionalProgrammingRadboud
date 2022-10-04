6f85168a39409fd442af4d78b0d12931ae02eb19
NemuruYama <nemuruyama@gmail.com>
2022-10-04 15:35:55 +0200

    5.5

--- Assignments/Assignment_5/Folders.hs
+++ Assignments/Assignment_5/Folders.hs
@@ -2,17 +2,24 @@ module Folders where
 
 import Prelude hiding (and,or,elem,maximum)
 
---and :: [Bool] -> Bool
+and :: [Bool] -> Bool
+and = foldr (&&) True
 
---or :: [Bool] -> Bool
+or :: [Bool] -> Bool
+or = foldr (||) False
 
---elem :: (Eq a) => a -> [a] -> Bool
+elem :: (Eq a) => a -> [a] -> Bool
+elem n = foldr (\x -> (||) (n == x)) False
 
---maximum :: (Ord a) => [a] -> a
+maximum :: (Ord a) => [a] -> a
+maximum [] = error "An empty list has no maxmium"
+maximum xs = foldl1 max xs
 
---fromList :: (Ord a) => [a] -> Tree a
+fromList :: (Ord a) => [a] -> Tree a
+fromList = foldr insert Leaf
 
---fromBits :: [Integer] -> Integer
+fromBits :: [Integer] -> Integer
+fromBits = foldr (\x acc -> 2 * acc + x) 0
 
 {- -------------------------------------------------------------------}
 
