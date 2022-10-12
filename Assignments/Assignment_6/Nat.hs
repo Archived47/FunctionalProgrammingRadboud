dcf853d45edfb274c6183663f65dc41693237a81
NemuruYama <nemuruyama@gmail.com>
2022-10-12 11:01:52 +0200

    6.2

--- Assignments/Assignment_6/Nat.hs
+++ Assignments/Assignment_6/Nat.hs
@@ -3,20 +3,29 @@ module Nat where
 data Nat = O | S Nat
   deriving (Show)
 
---fromNat :: (Num t) => Nat -> t
---fromNat O     = ...
---fromNat (S x) = ...
+fromNat :: (Num t) => Nat -> t
+fromNat O     = 0
+fromNat (S x) = fromNat x + 1
 
---toNat :: (Ord t, Num t) => t -> Nat
+toNat :: (Ord t, Num t) => t -> Nat
+toNat 0 = O
+toNat n = S (toNat (n - 1))
 
---instance Eq Nat where 
---  ...
+instance Eq Nat where 
+  O == O = True
+  O == (S x) = False
+  (S x) == O = False
+  (S x) == (S y) = x == y
 
---instance Ord Nat where 
---  ...
+instance Ord Nat where 
+  O <= O = True
+  O <= (S x) = True
+  (S x) <= O = False
+  (S x) <= (S y) = x <= y
 
 instance Enum Nat where
-  succ x     = error "TODO"
-  pred x     = error "TODO"
-  toEnum x   = error "TODO"
-  fromEnum x = error "TODO"
+  succ x = S x
+  pred O = O
+  pred (S x) = x
+  toEnum x = toNat x
+  fromEnum x = fromNat x
