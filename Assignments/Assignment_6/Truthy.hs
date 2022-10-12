61d7f8989b687d08523f084cafb35a5e5ceb6f0a
NemuruYama <nemuruyama@gmail.com>
2022-10-12 11:19:19 +0200

    6.3

--- Assignments/Assignment_6/Truthy.hs
+++ Assignments/Assignment_6/Truthy.hs
@@ -1,10 +1,27 @@
 module Truthy where
 
--- class Truthy ...
+data Nope = Nope
+  deriving (Show)
 
--- instance Truthy ... where
+class Truthy a where
+  truthy :: a -> Bool
 
--- (&&&) :: ...
--- (|||) :: ...
---- ifThenElse :: ...
+instance Truthy Bool where
+  truthy x = x
+
+instance Truthy Int where
+  truthy x = x > 0
+
+instance Truthy Nope where
+  truthy x = False
+
+instance (Truthy a, Truthy b) => Truthy (a, b) where
+  truthy (x, y) = truthy x || truthy y
+
+(&&&) :: (Truthy a, Truthy b) => a -> b -> Bool
+x &&& y = truthy x && truthy y
+(|||) :: (Truthy a, Truthy b) => a -> b -> Bool
+x ||| y = truthy x || truthy y
+ifThenElse :: (Truthy a) => a -> b -> b -> b
+ifThenElse x y z = if truthy x then y else z
 
