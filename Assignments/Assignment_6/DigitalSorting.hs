7dfbf04ea36c22ff583bd9f571fbb03c1c3244ab
NemuruYama <nemuruyama@gmail.com>
2022-10-16 14:02:06 +0200

    Some testing for uncons

--- Assignments/Assignment_6/DigitalSorting.hs
+++ Assignments/Assignment_6/DigitalSorting.hs
@@ -3,6 +3,7 @@ module DigitalSorting where
 import Data.List
 import Data.Char
 import Deck
+import Data.Maybe
 
 class Rankable key where
   rank :: [(key,a)] -> [[a]]
@@ -14,7 +15,7 @@ digitalSort :: (Rankable key) => [key] -> [key]
 digitalSort = digitalSortOn id
 
 genericRank :: (Ord key) => [(key,a)] -> [[a]]
-genericRank = map (map snd) . groupBy (\(a0, b0) (a1, b1) -> a0 == a1) . sortOn fst
+genericRank = map (map snd) . groupBy (\(a0, _) (a1, _) -> a0 == a1) . sortOn fst
 -- genericRank :: [Card] -> [[Value]]
 -- genericRank = map (map value) . groupBy (\x y -> suit x == suit y) . sortOn suit
 
@@ -30,9 +31,21 @@ instance Rankable Char where
 instance Rankable Bool where
   rank xs = [[value | (key, value) <- xs, not key], [value | (key, value) <- xs, key]]
 
--- instance (Rankable key1, Rankable key2) => Rankable (key1,key2) where
+instance (Rankable key1, Rankable key2) => Rankable (key1, key2) where
+  rank = concatMap rank . rank . map assoc
+    where
+      assoc :: (Rankable key1, Rankable key2) => ((key1,key2),a) -> (key1,(key2,a))
+      assoc ((k1, k2), a) = (k1, (k2, a))
 
---etc.
+instance (Rankable key) => Rankable (Maybe key) where
+  rank [] = []
+  rank xs = ([value | (Nothing, value) <- xs]) : rank [(key, value) | (Just key, value) <- xs]
+
+instance (Rankable key) => Rankable [key] where
+  rank [] = []
+  rank xs = []
+  -- uncons -> Turns "hello" into Just ('h',"ello")
+  -- This gets sent to the Maybe rank, which uses the Tuple rank
 
 ----------------------------------------------------------------------------------------------------
 -- some test inputs (it would be reasonably for "rank" and "genericRank" to produce the same output)
@@ -51,3 +64,11 @@ tupleTest = [ ((isLetter c, c), c) | c <- testPhrase ]
 
 listTest :: [(String,Char)]
 listTest = [ (w, c) | w <- groupBy (\x y->isLetter x==isLetter y) testPhrase, c <- w ]
+
+x y = initial (uncons "Hello")
+  where
+    f :: Maybe (a, [a]) -> Maybe (a, Maybe (a, [a]))
+    f Nothing = Nothing
+    f (Just (key, value)) = Just (key, uncons value)
+    initial Nothing = Nothing
+    initial (Just (key, value)) = Nothing
