d2826a8d82324e03c077628f2fe9be7c1ca8d3a6
NemuruYama <nemuruyama@gmail.com>
2022-10-22 19:28:30 +0200

    Initial commit + 7.4-7.7

--- Assignments/Assignment_7/Huffman.hs
+++ Assignments/Assignment_7/Huffman.hs
@@ -1,6 +1,7 @@
 module Huffman where
 
 import Data.List
+import Debug.Trace
 
 data Btree a = Tip a | Bin (Btree a) (Btree a)
   deriving Eq -- Show is done manually
@@ -11,20 +12,63 @@ data Bit = O | I
 -----------------------------------------------------------------------
 
 frequencies :: (Ord a) => [a] -> [(a,Int)]
-frequencies = error "TODO: IMPLEMENT ME"
+frequencies = map (\x -> (head x, length x)) . group . sort
 
 -----------------------------------------------------------------------
 
-huffman :: [(a,Int)] -> Btree a
-huffman = error "TODO: IMPLEMENT ME"
+-- Left it like this as this was my thougt process, can be optimized and written down on a single line
+huffman :: [(a, Int)] -> Btree a
+huffman = head . map fst . step3
+  where
+    step1 = map (\(x,i) -> (Tip x, i))
+    step2 = sortOn snd . step1
+    step3 = step3Helper . sortOn snd . step2
+    step3Helper :: [(Btree a, Int)] -> [(Btree a, Int)]
+    step3Helper [] = []
+    step3Helper [x] = [x]
+    step3Helper (x:y:ys) = step3Helper $ sortOn snd ((Bin (fst x) (fst y), snd x + snd y) : ys)
 
 -----------------------------------------------------------------------
 
---encode :: (Ord a) => Btree a -> [a] -> [Bit]
+codes :: Btree a -> [(a, [Bit])]
+codes (Tip x) = [(x, [O])]
+codes tree = sortBy (\(x, xbits) (y, ybits) -> if length xbits >= length ybits then GT else LT) $ codesHelper (tree, [])
+ where
+  codesHelper :: (Btree a, [Bit]) -> [(a, [Bit])]
+  codesHelper (Tip a, bits) = [(a, reverse bits)]
+  codesHelper (Bin (Tip x) (Tip y), bits) = codesHelper (Tip x, O : bits) ++ codesHelper (Tip y, I : bits)
+  codesHelper (Bin (Tip x) (Bin y1 y2), bits) = codesHelper (Tip x, O : bits) ++ codesHelper (Bin y1 y2, I : bits)
+  codesHelper (Bin (Bin x1 x2) (Tip y), bits) = codesHelper (Bin x1 x2, O : bits) ++ codesHelper (Tip y, I : bits)
+  codesHelper (Bin (Bin x1 x2) (Bin y1 y2), bits) = codesHelper (Bin x1 x2, O : bits) ++ codesHelper (Bin y1 y2, I : bits)
+
+encode :: (Ord a) => Btree a -> [a] -> [Bit]
+encode tree [] = []
+encode tree (x:xs) = case find (\ (f, bits) -> x == f) ass of
+  Nothing -> encode tree xs
+  Just (x, bits) -> bits ++ encode tree xs
+  where
+    ass = codes tree
 
 -----------------------------------------------------------------------
 
---decode :: (Ord a) => Btree a -> [Bit] -> [a]
+decode :: (Ord a) => Btree a -> [Bit] -> [a]
+decode tree xs = decodeHelper tree (xs, [])
+  where
+    ass = codes tree
+    -- findItem :: [Bit] -> [a]
+    findItem bs = case find (\ (f, bits) -> bs == reverse bits) ass of
+      Nothing -> []
+      Just (x, bits) -> [x]
+    -- decodeHelper :: (Ord a) => Btree a -> ([Bit], [Bit]) -> [a]
+    decodeHelper tree ([], []) = []
+    decodeHelper tree ([], retainer) = findItem retainer
+    decodeHelper tree (x:xs, retainer) =  if null result then decodeHelper tree (xs, x : retainer) else result ++ decodeHelper tree (xs, [])
+      where result = findItem (x : retainer)
+
+encodeTest = encode ct "hello world"
+  where ct = huffman (frequencies "hello world")
+decodeTest = decode ct encodeTest
+  where ct = huffman (frequencies "hello world")
 
 -----------------------------------------------------------------------
 
@@ -39,7 +83,7 @@ backus1978 =
   \still higher level ones in a style not possible in conventional languages."
 
 thanksForAllTheFish :: String
-thanksForAllTheFish = 
+thanksForAllTheFish =
   "It is an important and popular fact that things are not always what\n\
   \they seem. For instance, on the planet Earth, man had always\n\
   \assumed that he was more intelligent than dolphins because he had\n\
@@ -70,7 +114,7 @@ thanksForAllTheFish =
 -- a function which performs a rudimentary test to see if you get the correct frequencies for the above text
 testFrequencies :: Bool
 testFrequencies = (\x y->frequencies x == frequencies y) soLongAnd thanksForAllTheFish
-  where soLongAnd = concatMap (\(c,n)->replicate n c) $ frequencies (thanksForAllTheFish)
+  where soLongAnd = concatMap (\(c,n)->replicate n c) $ frequencies thanksForAllTheFish
 
 -- a function which performs a rudimentary test to see if you get the correct Huffman tree for (frequencies "hello world")
 testHuffman :: IO()
@@ -78,7 +122,7 @@ testHuffman = display mismatches
   where
   mismatches = filter (\(x,y)->huffman x /= y) testSet
   (==>) x y = (x,y) -- local syntactic sugar for tuples
-  testSet = [ [(' ',1)] 
+  testSet = [ [(' ',1)]
             ==> Tip ' '
             , [(' ',1),('d',1)]
             ==> Bin (Tip ' ') (Tip 'd')
@@ -107,7 +151,7 @@ instance (Show a) => Show (Btree a) where
   show = indent "\n"
     where
     indent _   (Tip x)   = "Tip " ++ show x
-    indent _   (Bin l@(Tip _) r@(Tip _)) 
+    indent _   (Bin l@(Tip _) r@(Tip _))
                          = "Bin (" ++ show l ++ ") (" ++ show r ++ ")"
     indent pre (Bin l r) = "Bin (" ++ indent (pre++"     ") l ++ ")" ++ pre ++
                            "    (" ++ indent (pre++"     ") r ++ ")"
