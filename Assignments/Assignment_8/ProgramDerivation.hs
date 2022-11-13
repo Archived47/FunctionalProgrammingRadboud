b319e2c767045497a45d1b4579fd557bcc9ccf3c
NemuruYama <nemuruyama@gmail.com>
2022-11-13 11:13:03 +0100

    I don't understand it

--- Assignments/Assignment_8/ProgramDerivation.hs
+++ Assignments/Assignment_8/ProgramDerivation.hs
@@ -12,8 +12,24 @@ inorder Leaf = []
 inorder (Node x lt rt) = inorder lt ++ [x] ++ inorder rt
 
 {-
+TODO, NO CLUE
 Derive
   inorderCat t xs = inorder t ++ xs
+
+  Case 1: t = Leaf
+  inorderCat Leaf xs
+  = inorder Leaf ++ xs
+  = [] ++ xs
+  = xs
+
+  Case 2: t = Node x l r
+  IH1: inorderCat l xs = inorder l ++ xs
+  IH2: inorderCat r xs = inorder r ++ xs
+  inorderCat t xs
+  = inorder (Node x l r) ++ xs
+  = (inorder l ++ [x] ++ inorder r) ++ xs 
+  = (inorder l ++ [x]) ++ (inorder r ++ xs)
+  = (inorder l ++ [x]) ++ (inorderCat r xs)
 -}
 
 inorderCat :: Tree a -> [a] -> [a]
@@ -23,6 +39,22 @@ inorder' :: Tree a -> [a]
 inorder' t = inorderCat t []
 
 -- TODO: make me more efficient, too
+{-
+
+Derive
+  elems (Node x lt rt) = x : elems lt ++ elems rt
+
+  Case 1: t = Leaf
+  elems Leaf
+  = []
+
+  Case 2: t = Node x lt rt
+  elems Node x lt rt
+  = x : elems lt ++ elems rt
+  = 
+  
+
+-}
 elems :: Tree a -> [a]
 elems Leaf = []
 elems (Node x lt rt) = x : elems lt ++ elems rt
