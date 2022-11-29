d535fde012e4ff8c8ec117457f0820ae43f605b5
NemuruYama <nemuruyama@gmail.com>
2022-11-29 21:11:10 +0100

    11.9(1.2)

--- Assignments/Assignment_11/AST.hs
+++ Assignments/Assignment_11/AST.hs
@@ -1,4 +1,5 @@
 module AST where
+import Result (Result)
 
 -- this template uses infix constructors; feel free to use AST2.hs (which uses prefix ones) if you prefer
 -- (if you really liked your own solution to Exercise 4.7, you can use that as well)
@@ -8,28 +9,27 @@ type Identifier = String
 data Expr = Lit Integer | Var Identifier | Expr :+: Expr | Expr :-: Expr | Expr :*: Expr | Expr :/: Expr
   deriving (Show)
 
-infixl 6 :+: 
-infixl 6 :-: 
+infixl 6 :+:
+infixl 6 :-:
 infixl 7 :/:
 infixl 7 :*:
 
-eval :: (Fractional a, Eq a) => Expr -> [(Identifier,a)] -> Maybe a 
-eval (Lit k) _ = Just (fromInteger k) 
-eval (x :+: y) vars = case (eval x vars, eval y vars) of 
-                     (Just x', Just y') -> Just (x'+y')
-                     _ -> Nothing
-                       
-eval (x :-: y) vars = case (eval x vars, eval y vars) of 
-                     (Just x', Just y') -> Just (x'-y')
-                     _ -> Nothing
+eval :: (Fractional a, Eq a) => Expr -> [(Identifier,a)] -> Maybe a
+eval (Lit k) _ = Just (fromInteger k)
+eval (x :+: y) vars = (+) <$> eval x vars <*> eval y vars
+eval (x :-: y) vars = (-) <$> eval x vars <*> eval y vars
+eval (x :*: y) vars = (*) <$> eval x vars <*> eval y vars
+eval (x :/: y) vars = case (eval x vars, eval y vars) of
+  (_, Just 0)  -> Nothing
+  (Just x', Just y') -> Just (x'/y')
+  _ -> Nothing
 
-eval (x :*: y) vars = case (eval x vars, eval y vars) of
-                     (Just x', Just y') -> Just (x'*y') 
-                     _ -> Nothing 
+eval (Var name) vars = lookup name vars
 
-eval (x :/: y) vars = case (eval x vars, eval y vars) of 
-                     (Just  _, Just 0)  -> Nothing
-                     (Just x', Just y') -> Just (x'/y') 
-                     _ -> Nothing
-
-eval (Var name) vars = error "FIXME: variable support"
+vars :: (Fractional a, Eq a) => [(String, a)]
+vars = [("x",5), ("y",37), ("z", 3)]
+expr1 = eval (Var "x" :+: Var "y") vars
+expr2 = eval (Var "y" :-: Var "x") vars
+expr3 = eval (Var "x" :+: Var "y") []
+expr4 = eval (Var "z" :*: Lit 2) vars
+expr5 = eval (Var "z" :/: ((Lit 2) :-: (Lit 2))) vars
